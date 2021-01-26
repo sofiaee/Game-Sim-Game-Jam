2b6eab6e4b743908fda65675552f1755a6eef86c
sofiaee <65104306+sofiaee@users.noreply.github.com>
2021-01-26 17:43:08 -0600

    Added Player Color Switch Ability

---
+++ Game Sim Game Jam Project/Assets/Scripts/SwitchPower.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPower : MonoBehaviour
+{
+
+    public bool isLight;
+
+    private Animator anim;
+    private AnimatorOverrideController overrider;
+    public AnimationClip[] lightAnimationClips;
+    public AnimationClip[] darkAnimationClips;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+        overrider = new AnimatorOverrideController(anim.runtimeAnimatorController);
+        anim.runtimeAnimatorController = overrider;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+
+            if(isLight)
+            {
+                isLight = false;
+                overrider["Light_Player_Idle"] = darkAnimationClips[0];
+                overrider["Light_Player_Run"] = darkAnimationClips[1];
+                overrider["Light_Player_Jump"] = darkAnimationClips[2];
+
+            }
+            else
+            {
+                isLight = true;
+                overrider["Light_Player_Idle"] = lightAnimationClips[0];
+                overrider["Light_Player_Run"] = lightAnimationClips[1];
+                overrider["Light_Player_Jump"] = lightAnimationClips[2];
+            }
+        }
+        
+    }
+
+}
